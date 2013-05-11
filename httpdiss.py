02e9d071c66b91fee75c751af66fb2c3cfc8dc23
otavioarj <otavioarj@gmail.com>
2013-05-11 18:04:59 -0300

    Working release of HTTP dissector

--- httpdiss.py
+++ httpdiss.py
@@ -4,8 +4,7 @@
 # At Public Domain if, and only if, the author remain unchanged or a clear reference is made to him =]
 # This code completely depends on Scapy http://www.secdev.org/projects/scapy 
 
-from threading import Thread
-import sys, logging, re
+import sys, logging, re, time
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
@@ -19,27 +18,30 @@ class hstream:
 
 #HTTP Data
 def hdata(data):
-	hparse=re.search('\r\n\r\n(.+?)', data)
-	if hparse:
-		rdata=hparse.group(1) # Data, just after the HTTP Response Header
-		return rdata
-#	else
-#		return ''
+	rdata=data.split('\r\n\r\n',1)
+	#print rdata[1]
+	if len(rdata)>1:
+		return rdata[1]
+	else:
+		return ' '
 
 
 
 #HTTPRequest Handler
 def hrequest (src,dst,ack,data):
-	hparse=re.search('/(.+?) HTTP/1.1\z', data)
-	if not hparse:		
-		hparse=re.search(' ^/(.+?) HTTP', data)
-	if not hparse:
-		fname='none'
+	hparse=re.search('/(.+?) HTTP/1.1', data)	
+	if hparse:
+		fname=hparse.group(1).split('/')
+		fname=fname[len(fname)-1]
 	else:
-		fname=hparse.group(1)
-		print fname
+		hparse=re.search(' ^/(.+?) HTTP', data)
+		if not hparse:
+			fname='none'	
+		else:
+			fname=hparse.group(1)
+	fname=fname.split('?',2)[0] 
 	newconn=hstream()
-	newconn.hfile= open(str(src)+"-"+str(dst)+"."+fname, "a")
+	newconn.hfile= open(str(src)+"-"+str(dst)+"."+str(fname), "a")
 	if data.find('PUT')==0:
 		rdata=hdata(data)
 		newconn.hfile.write(rdata)
@@ -50,49 +52,50 @@ def hrequest (src,dst,ack,data):
 	newconn.nextseq=ack # Remember that this Ack is the last server side TCP sequence number
 	hsession.append(newconn)
 
-def hresponse(seq,data):
-	for i in range(0,len(hsession)):
+def hresponse(seq,data,isdata):
+	for i in range(len(hsession),0):
 		#print  "%d e %d"%(hsession[i].nextseq, seq)
 		if hsession[i].nextseq == seq:
-			print "MATCHING!!!!"
-			rdata=hdata(data)
-			if rdata:
-				if '206 (Partial Content)' in data: # Server accepts Partial Content
-					hsession[i].hfile.seek(hsession[i].frange)
-					hsession[i].frange+=len(rdata)
-				hsession[i].hfile.write(rdata)
-			hsession[i].nextseq+=len(data) # Found HTTP Data or not, the next TCP seq from server side is always increased by TCP data length
+			if isdata:
+				rdata=hdata(data)
+				#time.sleep(2)
+				if rdata:
+					if '206 (Partial Content)' in data: # Server accepts Partial Content
+						hsession[i].hfile.seek(hsession[i].frange)
+						hsession[i].frange+=len(rdata)
+					if '141.0.168.7' in hsession[i].hfile.name:
+						print "\n Isso\n "+  data+ " \ndeu nisso\n"+rdata # conxao continua viva, assim o prox seq é na verdade *outro* arquivo
+					hsession[i].hfile.write(rdata)
+			else:
+				hsession[i].hfile.write(data)
+			hsession[i].nextseq+=len(data) # Found HTTP Data or not, the next TCP seq from server side is always increased by TCP data length 
 
 #Packet Analyser 
 def pkgan(pkt):
-	global dseq
 	if not(TCP in pkt): # forcing check as tcp in filter isn't enough, for some reason....  
 		return
 	raw= pkt[TCP].payload
 	data = str(raw)
-	if not('HTTP' in data):
-		return
-	header=data.splitlines()[0]
-	rsqt=['OPTIONS','GET','HEAD','POST','PUT','DELETE','TRACE','CONNECT'] # only GET, HEAD and PUT can be used to transfer files
+	if 'HTTP' in data:
+		header=data.splitlines()[0]
+		rsqt=['OPTIONS','GET','HEAD','POST','PUT','DELETE','TRACE','CONNECT'] # only GET, HEAD and PUT can be used to transfer files
 																																				# the others request parameters is there only for algorithm correctness
-	#print raw
-	if any(x in header for x in rsqt): # Search for HTTP requests
-		hrequest( pkt[IP].src,pkt[IP].dst,pkt[TCP].ack,data)
-		#print "Req  %s %d"%(pkt[IP].src,pkt[TCP].ack)
+		if any(header.find(x)==0 for x in rsqt): # Search for HTTP requests
+			hrequest( pkt[IP].src,pkt[IP].dst,pkt[TCP].ack,data)
+		else: # It's a HTTP responses, as it have a HTTP Header, but not a request ones
+			hresponse(pkt[TCP].seq,data,True)
+	else: # It can be a HTTP response, that will be confirmed only if TCP seqs matches, as it may contain only file data.
+		hresponse(pkt[TCP].seq,data,False)
 
-	elif header.find('HTTP/')==0:  # Search for HTTP responses
-		#print "Req  %d"%(pkt[TCP].seq)
-		hresponse(pkt[TCP].seq,data)
 
 
 
 def main2():
 	if len(sys.argv) < 2:
-		print "[*] Usage:", sys.argv[0] ," somefile.pcap"
+		print "[*] Usage:", sys.argv[0] ," somefile.cap"
 		sys.exit(0)
 
 	fpcap=sys.argv[1]
-	print fpcap
 	global hsession
 	hsession=[]
 	try:
